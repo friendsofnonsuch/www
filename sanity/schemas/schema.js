8f8024fd31d1dfc44a2a3c3aebf637d841784dae
Richard Herbert <richard@infoweb.co.uk>
2021-04-11 11:51:06 +0100

    Removed Events from Sanity Desk

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -14,7 +14,7 @@ export default createSchema( {
 	// Then proceed to concatenate our document type
 	// to the ones provided by any plugins that are installed
 	types: schemaTypes.concat( [
-		event
-		,link
+		link
+		// ,event
 	] ),
 } )
