fe973089fcc0df0b51af0649bb594f100049309f
Richard Herbert <richard@infoweb.co.uk>
2022-03-09 16:18:08 +0000

    Added UTC plugin

--- src/_data/events.js
+++ src/_data/events.js
@@ -2,9 +2,11 @@ require( 'dotenv' ).config();
 
 let dayjs = require( 'dayjs' );
 var AdvancedFormat = require( 'dayjs/plugin/advancedFormat' );
+var utc = require( 'dayjs/plugin/utc' );
 var Timezone = require( 'dayjs/plugin/timezone' );
 
 dayjs.extend( AdvancedFormat );
+dayjs.extend( utc );
 dayjs.extend( Timezone );
 
 const sanityClient = require( '@sanity/client' );
@@ -26,8 +28,8 @@ module.exports = async function() {
 			return response.map( event => {
 				return {
 					date: dayjs( event.date ).format( 'dddd, Do MMMM' )
-					,time: dayjs( event.date ).format( 'h.mma', 'Europe/London' )
-					,speaker: event.date
+					,time: dayjs( event.date ).tz( 'Europe/London' ).format( 'h.mma' )
+					,speaker: event.speaker
 					,subject: event.subject
 				};
 			} );
