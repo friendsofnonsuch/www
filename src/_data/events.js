b4690e3c4ea8227456b13bd410b292b288f36df5
Richard Herbert <richard@infoweb.co.uk>
2024-09-12 11:53:42 +0100

    Added dynamic Event presentation

--- src/_data/events.js
+++ src/_data/events.js
@@ -1,41 +1,24 @@
 require( 'dotenv' ).config();
 
-let dayjs = require( 'dayjs' );
-var AdvancedFormat = require( 'dayjs/plugin/advancedFormat' );
-var utc = require( 'dayjs/plugin/utc' );
-var Timezone = require( 'dayjs/plugin/timezone' );
+const EleventyFetch = require( '@11ty/eleventy-fetch' );
 
-dayjs.extend( AdvancedFormat );
-dayjs.extend( utc );
-dayjs.extend( Timezone );
+switch ( process.env.ELEVENTY_ENV ) {
+	case 'development':
+		url = 'http://admin.friendsofnonsuch.localhost/api/events/';
+	break;
 
-const sanityClient = require( '@sanity/client' );
-
-const client = sanityClient( {
-	projectId: process.env.SANITY_PROJECTID
-	,dataset: process.env.SANITY_DATASET
-	,token: process.env.SANITY_TOKEN
-	,apiVersion: '2021-03-25'
-	,useCdn: false
-} );
-
-
-module.exports = async function() {
-	const query = '*[ _type == "event" ] | order( date asc )'
-
-	return await client.fetch( query, {} )
-		.then( response => {
-			return response.map( event => {
-				return {
-					date: dayjs( event.date ).format( 'dddd, Do MMMM' )
-					,time: dayjs( event.date ).tz( 'Europe/London' ).format( 'h.mma' )
-					,speaker: event.speaker
-					,subject: event.subject
-					,isStableWing: event.isStableWing
-				};
-			} );
-		} )
-		.catch(
-			error => console.error( error )
-		);
+	default:
+		url = 'http://admin.friendsofnonsuch.co.uk/api/events/';
 }
+
+module.exports = async function () {
+	return EleventyFetch( url, {
+		duration: '0d'
+		,type: 'json'
+		,fetchOptions: {
+			headers: {
+				'Authorization': 'Bearer ' + process.env.BEARER_TOKEN
+			}
+		}
+	} );
+};
