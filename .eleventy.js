58a5907322ffc0601a931cacc7a216f0a08891e2
Richard Herbert <richard@infoweb.co.uk>
2025-05-23 15:17:03 +0100

    Added markdown anchor

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,17 @@
+import markdownIt from 'markdown-it';
+// import markdownItAnchor from 'markdown-it-anchor';
+
 export default async function( eleventyConfig ) {
+	let options = {
+		html: true,
+		breaks: true,
+		linkify: true,
+	};
+
 	eleventyConfig.addGlobalData( 'layout', 'layouts/default.html' );
+
+	eleventyConfig.setLibrary( 'md', markdownIt( options ) );
+
+	eleventyConfig.amendLibrary( 'md', ( mdLib ) => mdLib.use( markdownIt ) );
+	// eleventyConfig.amendLibrary( 'md', ( mdLib ) => mdLib.use( markdownItAnchor ) );
 };
