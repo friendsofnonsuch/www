425133e1b71499a4758de0a4fe4a2641217d6901
Richard Herbert <richard@infoweb.co.uk>
2026-05-12 19:20:43 -0700

    Uninstalled Sanity

--- src/_data/links.js
+++ src/_data/links.js
@@ -1,14 +1,5 @@
 require( 'dotenv' ).config();
 
-const sanityClient = require( '@sanity/client' )
-const client = sanityClient( {
-	projectId: process.env.SANITY_PROJECTID
-	,dataset: process.env.SANITY_DATASET
-	,token: process.env.SANITY_TOKEN
-	,apiVersion: '2021-03-25'
-	,useCdn: false
-} );
-
 module.exports = async function() {
 	const query = '*[ _type == "link"] | order( order asc )'
 
